9e942bb76c231f3ad7892b4faa2084c05dccc240
NachoSoto <ignaciosoto90@gmail.com>
2015-08-02 15:00:53 +0000

    Improved VideoFetcher dummy data

--- core/src/VideoFetcher.cpp
+++ core/src/VideoFetcher.cpp
@@ -7,14 +7,36 @@ namespace Khan {
 		return std::make_shared<Core::VideoFetcherImpl>();
 	}
 
-namespace Core {
-	VideoFetcherImpl::VideoFetcherImpl() {
-	}
+	namespace Core {
+		VideoFetcherImpl::VideoFetcherImpl() {
+		}
 
-	vector<Video> VideoFetcherImpl::fetchVideos() {
-		return vector<Video>{
-			Video(false, {{"one", "1"}, {"two", "2"}}, "test", 2, { "test", "test" }, "title", "description", ContentType::VIDEO)
-		};
+		vector<Video> VideoFetcherImpl::fetchVideos() {
+			return {
+				Video(false,
+					  {
+						  {"mp4", "mp4 URL"},
+						  {"png", "thumbnail"}
+					  },
+					  "http://www.youtube.com/watch?v=dQw4w9WgXcQ",
+					  522,
+					  {"Sal Khan", "Nacho Soto"},
+					  "Adding/subtracting negative numbers",
+					  "Adding and subtracting negative numbers",
+					  ContentType::VIDEO
+					  ),
+				Video(true,
+					  {
+						  {"mp4", "mp4 URL"},
+					  },
+					  "http://www.youtube.com/watch?v=C38B33ZywWs",
+					  100,
+					  {"Sal Khan"},
+					  "Android Programming",
+					  "We made an Android app and you won't believe what happened next",
+					  ContentType::VIDEO
+					  ),
+			};
+		}
 	}
-}
 }
